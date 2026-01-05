b6de7df2e6c6135e5a892de3491b3ec051d6b0c0
RezaRahmanAI <rizverahman.ai@gmail.com>
2026-01-05 07:24:25 +0600

    Exeption middleware

--- Ecom.Api/Program.cs
+++ Ecom.Api/Program.cs
@@ -1,3 +1,4 @@
+using Ecom.Api;
 using Ecom.Application;
 using Ecom.Infrastructure;
 
@@ -13,6 +14,8 @@ builder.Services.AddApplicationServices();
 builder.Services.AddInfrastructure(builder.Configuration);
 
 var app = builder.Build();
+
+app.UseMiddleware<ExceptionHandlingMiddleware>();
 
 app.UseSwagger();
 app.UseSwaggerUI();
