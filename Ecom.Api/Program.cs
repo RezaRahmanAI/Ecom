169c7b20cfd5ca36e16a3b9d441d0bd83e9ee666
RezaRahmanAI <rizverahman.ai@gmail.com>
2026-01-05 22:41:23 +0600

    running

--- Ecom.Api/Program.cs
+++ Ecom.Api/Program.cs
@@ -1,16 +1,45 @@
 using Ecom.Api;
 using Ecom.Api.Validators.Products;
 using Ecom.Application;
+using Ecom.Domain.Entities;
 using Ecom.Infrastructure;
 using FluentValidation;
+using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+
+
+
 builder.Services.AddControllers();
 
 // Fluent Validation
+
+builder.Services.Configure<ApiBehaviorOptions>(options =>
+{
+    options.InvalidModelStateResponseFactory = context =>
+    {
+        var errors = context.ModelState
+            .Where(x => x.Value?.Errors.Count > 0)
+            .ToDictionary(
+                kvp => kvp.Key,
+                kvp => kvp.Value!.Errors.Select(e => e.ErrorMessage).ToArray()
+            );
+
+        var response = new AppErrorResponse
+        {
+            Success = false,
+            StatusCode = StatusCodes.Status400BadRequest,
+            Message = "Validation failed",
+            TraceId = context.HttpContext.TraceIdentifier,
+            Errors = errors
+        };
+
+        return new BadRequestObjectResult(response);
+    };
+});
 
 builder.Services.AddValidatorsFromAssemblyContaining<CreateProductRequestValidator>();
 
